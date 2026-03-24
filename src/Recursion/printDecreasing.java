c3ced019503fd2e2090e82619c4bafb3412675e6
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-03-25 00:39:41 +0530

    add

--- src/Recursion/printDecreasing.java
+++ src/Recursion/printDecreasing.java
@@ -1,4 +1,22 @@
 package Recursion;
 
+import java.net.StandardSocketOptions;
+import java.util.Scanner;
+
 public class printDecreasing {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter num: ");
+        int n = scn.nextInt();
+        System.out.print("Decreased number: ");
+        decrease(n);
+
+    }
+    public static void decrease(int n){
+        if(n == 0){
+            return ;
+        }
+        System.out.println(n);
+        decrease(n - 1);
+    }
 }
