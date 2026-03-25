0c30f251bccf8fb57961bc0ebabfe0f4f76b39ed
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-03-25 08:07:23 +0530

    pdi

---
+++ src/Recursion/printDecreasingIncreasing.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class printDecreasingIncreasing {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pdi(n);
+    }
+
+    public static void pdi(int n){
+        if(n < 1){
+            return;
+        }
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+
+
+}
