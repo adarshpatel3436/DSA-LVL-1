7387aa3fcfc6cbaa237d4ec2ab7861158f2575c0
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-03-25 08:41:00 +0530

    CalculatePower

---
+++ src/Recursion/CalculatePower.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class CalculatePower {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int xn = power(x , n);
+        System.out.println(xn);
+    }
+    public static int power (int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        int xnm1 = power(x , n-1);
+        int xn = x * xnm1;
+        return xn;
+    }
+}
