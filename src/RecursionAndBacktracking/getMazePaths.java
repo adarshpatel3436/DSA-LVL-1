7f1754c1bde06ef4ad91ec081d0be8fe2860d4b1
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-01 00:38:52 +0530

    getMazePaths

--- src/RecursionAndBacktracking/getMazePaths.java
+++ src/RecursionAndBacktracking/getMazePaths.java
@@ -6,13 +6,39 @@ import java.util.Scanner;
 public class getMazePaths {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
+        ArrayList<String> paths = getMazePaths(1, 1, n, m);
+        System.out.println(paths);
     }
      // sr - source row
      // sc - source column
      // dr - destination row
      // dc - destination column
     public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
-        return null;
+        if(sr == dr && sc == dc){
+            ArrayList<String> Bpaths = new ArrayList<>();
+            Bpaths.add("");
+            return Bpaths;
+        }
+
+        ArrayList<String> hpaths = new ArrayList<>();
+        ArrayList<String> vpaths = new ArrayList<>();
+        if(sr < dr){
+            vpaths = getMazePaths(sr + 1, sc, dr, dc);
+        }
+        if(sc < dc){
+            hpaths = getMazePaths(sr, sc + 1, dr, dc);
+        }
+
+        ArrayList<String> paths = new ArrayList<>();
+        for(String hpath : hpaths){
+            paths.add("h" + hpath);
+        }
+        for(String vpath : vpaths){
+            paths.add("v" + vpath);
+        }
+        return paths;
     }
 }
