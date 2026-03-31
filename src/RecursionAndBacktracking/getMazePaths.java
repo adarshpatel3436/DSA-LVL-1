ca4018716d5c39dc152fd89c923a13b34e598d5d
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-01 00:19:16 +0530

    file add

---
+++ src/RecursionAndBacktracking/getMazePaths.java
@@ -0,0 +1,18 @@
+package RecursionAndBacktracking;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class getMazePaths {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+    }
+     // sr - source row
+     // sc - source column
+     // dr - destination row
+     // dc - destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        return null;
+    }
+}
