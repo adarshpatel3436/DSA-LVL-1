79a64abba780e6997dc27e822c3ef8e9d2b2af5a
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-17 07:31:06 +0530

    update

--- src/Array1D/arrayInitialize.java
+++ src/Array1D/arrayInitialize.java
@@ -4,21 +4,42 @@ public class arrayInitialize {
     public static void main(String[] args) {
 //        int[] one;
 //        one = new int[5];
-        int[] one = new int[5];
-        one[0] = 22;
-        one[1] = 55;
-        one[2] = 43;
-        one[3] = 66;
-        one[4] = 77;
+        // or is tarah kar sakte h
+
+//        int[] one = new int[5];
+//        one[0] = 22;
+//        one[1] = 55;
+//        one[2] = 43;
+//        one[3] = 66;
+//        one[4] = 77;
 
 //        int[] two = one;
 //        two[3] = 234;
-        swap(one,0,4);
+//        for (int i = 0; i < one.length; i++) {
+//            System.out.println(one[i]);                               // 22 55 43 234 77
+//        }
+
+//        for (int i = 0; i < one.length; i++) {
+//            System.out.println(two[i]);                             // 22 55 43 234 77    value change ho jayegi kyuki dono ek hi array ko point kar rhe h
+//        }
+
+        //        System.out.println(two[3]);                   // 234
+//        System.out.println(two.length);
+
+
+
+
+        int[] one = new int[4];
+        one[0] = 10;
+        one[1] = 20;
+        one[2] = 30;
+        one[3] = 50;
+
+        swap(one,0,3);
         for (int i = 0; i < one.length; i++) {
             System.out.println(one[i]);
         }
-//        System.out.println(two[3]);
-//        System.out.println(two.length);
+     // swaped
 
     }
     public static void swap(int[] arr , int i , int j){
