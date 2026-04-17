d928b8b635d3cc9c674c3bb04b0a5fce2007d90c
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-17 08:16:13 +0530

    update barChart

--- src/Array1D/barChart.java
+++ src/Array1D/barChart.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class barChart {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter length of array: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
+
+
         int max = arr[0];
         for(int i = 0; i< arr.length ; i++){
             if(arr[i] > max){
