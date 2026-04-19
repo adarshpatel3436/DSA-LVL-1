22a241340f17bfaea0cf8aa66157724a8b812ca7
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-19 19:10:17 +0530

    inverseAnArray

---
+++ src/Array1D/subarrayOfArray.java
@@ -0,0 +1,24 @@
+package Array1D;
+
+import java.util.Scanner;
+
+public class subarrayOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
