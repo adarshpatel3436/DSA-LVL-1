fee7aaa76e0f7fd1642e949f70fabdf5b31c70e4
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-04-30 17:50:57 +0530

    merge two Sorted array

---
+++ src/TimeAndSpace/mergeTwoSortedArray.java
@@ -0,0 +1,63 @@
+package TimeAndSpace;
+
+import java.util.Scanner;
+
+public class mergeTwoSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1st array: ");
+        int m = sc.nextInt();
+        int[] arr1 = new int[m];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println("2nd array: ");
+        int n = sc.nextInt();
+        int[] arr2 = new int[n];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        int[] res = mergeTwoSortedArrays(arr1, arr2);
+        display(res);
+    }
+
+    public static int[] mergeTwoSortedArrays(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] < arr2[j]){
+                res[k] = arr1[i];
+                i++;
+                k++;
+            }else{
+                res[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i < arr1.length){
+            res[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while( j < arr2.length){
+            res[k] = arr2[j];
+            j++;
+            k++;
+        }
+        return res;
+    }
+
+    public static void display(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
