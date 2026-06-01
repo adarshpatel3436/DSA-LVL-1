3434a7b1c98d856fd1a3a780c704a1670d313517
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-06-02 01:05:50 +0530

    ll start

---
+++ src/LinkedList/BasicOfLinkedList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class BasicOfLinkedList {
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+    }
+}
