ff8d1e112d773dd01dbadfceecdb0e7eb35f2da0
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-06-02 23:36:40 +0530

    GetValueInLL

---
+++ src/LinkedList/GetValueInLL.java
@@ -0,0 +1,129 @@
+package LinkedList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GetValueInLL {
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = temp;
+                tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void display() {
+            for (Node temp = head; temp != null; temp = temp.next) {
+                System.out.print(temp.data + " ");
+            }
+            System.out.println();
+        }
+
+        public void removeFirst() {
+            if (size == 0) {
+                System.out.println("list is empty");
+            } else if (size == 1) {
+                head = tail = null;
+                size = 0;
+            } else {
+                head = head.next;
+                size--;
+            }
+        }
+
+        public int getFirst(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return head.data;
+            }
+        }
+        public int getLast(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return tail.data;
+            }
+        }
+        public int getAt(int idx){
+            if(size == 0) {
+                System.out.println("List is Empty");
+                return -1;
+            }else if(idx < 0 || idx >= size){
+                System.out.println("Invalid arg");
+                return -1;
+            }else{
+                Node temp = head;
+                for (int i = 0; i < idx; i++) {
+                    temp = temp.next;
+                }
+                return temp.data;
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+
+        while (str.equals("quit") == false) {
+
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("display")) {
+                list.display();
+            } else if (str.startsWith("size")) {
+                System.out.println(list.size());
+            } else if (str.startsWith("removeFirst")) {
+                list.removeFirst();
+            } else if (str.startsWith("getFirst")) {
+                System.out.println(list.getFirst());
+            } else if (str.startsWith("getLast")) {
+                System.out.println(list.getLast());
+            } else if (str.startsWith("getAt")) {
+                int idx = Integer.parseInt(str.split(" ")[1]);
+                System.out.println(list.getAt(idx));
+            }
+            str = br.readLine();
+        }
+    }
+}
+
+// input
+// addLast 10
+// addLast 20
+// addLast 30
+// getFirst
+// getLast
+// getAt 1
+// getAt 5
+// quit
