6cdab60092625b863ec241a5955e00aafdd82760
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-06-02 22:34:13 +0530

    displayAndSizeOfLL

---
+++ src/LinkedList/displayAndSizeOfLL.java
@@ -0,0 +1,68 @@
+package LinkedList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class displayAndSizeOfLL {
+
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if(size == 0){
+                head = temp;
+                tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public int size(){
+            return size;
+        }
+
+        public void display(){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+
+        while (str.equals("quit") == false) {
+
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            }
+
+            str = br.readLine();
+        }
+        list.display();
+        list.size();
+    }
+}
